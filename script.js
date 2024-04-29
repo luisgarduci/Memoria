15870001297db60344a946bef5809d823340cd6c
luisgarduci <112954236+luisgarduci@users.noreply.github.com>
2024-04-29 19:53:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const game_time = document.getElementById("game-time");
             arrayTurnedCards = [];
         }
             
-            }, 1000)
+            }, 500)
             
             }
             })
